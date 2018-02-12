32b6428e61be6339e323f52e3f5d411578d23868
Tien Nguyen <pri859@sudomail.com>
2018-02-12 22:57:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,6 @@ chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-var newLength = chocolateBars.unshift('element')
+function addElementToBeginningOfArray() {
+  var newLength = chocolateBars.unshift('element')
+}
