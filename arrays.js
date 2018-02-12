27d7827ad19f86eff65b6d87c8d4a65481936f9d
Tien Nguyen <pri859@sudomail.com>
2018-02-12 23:14:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,3 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray() {
   return ["foo", 1]
 }
-
-function destructivelyAddElementToBeginningOfArray() {
-  return chocolateBars.unshift('foo', 1)
-};
