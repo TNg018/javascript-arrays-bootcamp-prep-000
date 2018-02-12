9961652b9b273b58d2f019492c1abd0b9da412a4
Tien Nguyen <pri859@sudomail.com>
2018-02-12 22:33:33 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function accessElementInArray() {
+  return console.log(chocolateBars[n])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+return chocolateBars.shift(0)
+}
+
+function addElementToBeginningOfArray() {
+  return chocolateBars.unshift(chocolateBars, "element")
+}
