2beaadf439f46eb6850645b2bc77212f0c6b9e57
Tien Nguyen <pri859@sudomail.com>
2018-02-12 22:44:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 chocolateBars = [element]
-
-function removeElementFromEndOfArray() {
-  return chocolateBars.slice(0, chocolateBars.length - 1)
-}
