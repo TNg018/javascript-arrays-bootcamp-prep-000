8c969b54268d119e3d5d34988b1d517078ecde0a
Tien Nguyen <pri859@sudomail.com>
2018-02-12 23:51:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.psuh(element)
+  array.push(element)
 return array
 }
